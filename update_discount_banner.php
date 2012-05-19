347e44688a4271d0cb183d1cb5c2f3d3c3d505ff
Gennady Telegin <gtelegin@gmail.com>
2012-05-20 01:06:23 +0400

    Initial commit.
    Add update_discount_banner script.

---
+++ update_discount_banner.php
@@ -0,0 +1,63 @@
+<?php
+define('ROOT_DIR', './');
+
+require_once ROOT_DIR . 'nusoap/lib/nusoap.php';
+
+$wsdlurl = "http://soap.direct.yandex.ru/wsdl/v4/";
+$client = new nusoap_client($wsdlurl, 'wsdl'); 
+
+$client->authtype = 'certificate';
+$client->decode_utf8 = 0;
+$client->soap_defencoding = 'UTF-8';
+$client->certRequest['sslcertfile'] = './cert.crt'; 
+$client->certRequest['sslkeyfile'] = './private.key'; 
+$client->certRequest['cainfofile'] = './cacert.pem';
+
+$params = array(
+    'CampaignIDS' => array('1111111'), // place list of CampaingnIDS here
+    'GetPhrases' => 'WithPrices',
+);
+
+$result = $client->call('GetBanners', array('params' => $params));
+
+if (isset($result[0]) === false) {
+    die('Invalid Campaign IDs');
+}
+
+function addzero($s) {
+    if (strlen($s) < 2) {
+        $s = '0' . $s;
+    }
+
+    return $s;
+}
+
+$days = 1; // how much days add to current
+$t = localtime(time() + $days*24*60*60, true);
+$months = array('января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря');
+$anymonth = implode('|', $months);
+
+$banners = array();
+
+for ($i = 0, $n = count($result); $i < $n; ++$i) {
+    if ($result[$i]['IsActive'] == 'Yes') { // skip archived and moderated ads
+        $ad = $result[$i]['Text'];
+
+        $new_ad = preg_replace('/до (\d{2})\.(\d{2})/u', 'до ' . addzero($t['tm_mday']) . '.' . addzero($t['tm_mon'] + 1), $ad);
+
+        if ($new_ad == $ad) {
+            $new_ad = preg_replace("/до (\d{1,2}) ($anymonth)/u", 'до ' . addzero($t['tm_mday']) . ' ' . $months[$t['tm_mon']], $ad);
+        }
+
+        if ($new_ad != $ad) {
+            $result[$i]['Text'] = $new_ad;
+
+        //    echo "was: $ad\nnow: $new_ad\n\n";
+            $banners[] = $result[$i];
+        }
+    }
+}
+
+print_r($client->call('CreateOrUpdateBanners', array('params' => $banners)));
+print $client->getError();
+?>
